432817e1f100f08f787bf83c2a1da34d36389047
wodim <wodim@vortigaunt.net>
2021-09-16 13:41:16 +0200

    first commit

---
+++ bot.py
@@ -0,0 +1,100 @@
+import configparser
+import logging
+
+import telegram
+from telegram import Update
+from telegram.ext import CallbackContext, MessageHandler, Updater
+
+from hn import HN
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+ITEM_PERMALINK = 'https://news.ycombinator.com/item?id=%s'
+
+hn = HN()
+
+
+def _config(k: str) -> str:
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['bot'][k]
+
+
+def _e(text: str) -> str:
+    """escapes text with markdown v2 syntax"""
+    return telegram.utils.helpers.escape_markdown(text, 2)
+
+
+def _story_meta(story):
+    if 'score' in story:
+        yield '%s point%s' % (story['score'], 's' if story['score'] != 1 else '')
+    if 'descendants' in story:
+        yield '%s comment%s' % (story['descendants'], 's' if story['descendants'] != 1 else '')
+    elif 'kids' in story:
+        yield '%s comment%s' % (len(story['kids']), 's' if len(story['kids']) != 1 else '')
+
+
+def post_thread(chat_id: int, context: CallbackContext) -> None:
+    context.bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+
+    for story, state in hn.get_updates():
+        if state == 'killed':
+            header = 'FLAGGED'
+        elif state == 'resurrected':
+            header = 'UNFLAGGED'
+        else:
+            raise ValueError('unknown new state:', state)
+
+        text = '*%s: %s*' % (header, _e(story['title']))
+
+        if 'url' in story:
+            text += '\n' + _e(story['url'])
+
+        if (meta := _story_meta(story)):
+            text += '\n' + _e(' - '.join(meta))
+
+        if 'text' in story:
+            text += '\n\n' + _e(HN.clean_text(story['text']))
+
+        text += '\n\n' + _e(ITEM_PERMALINK % story['id'])
+
+        context.bot.send_message(chat_id, text,
+                                 parse_mode=telegram.constants.PARSEMODE_MARKDOWN_V2,
+                                 disable_web_page_preview=True)
+
+
+def cron(context: CallbackContext) -> None:
+    post_thread(int(_config('cron_chat_id')), context)
+
+
+def command_help(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Join %s' % _config('cron_chat_name'))
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(_config('token'))
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # reply to anything that is said to me in private
+    dispatcher.add_handler(MessageHandler(None, command_help))
+
+    # add a "cron" job that posts automatically every even hour
+    dispatcher.job_queue.run_repeating(cron, interval=10)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ hn.py
@@ -0,0 +1,119 @@
+import configparser
+import json
+import logging
+from time import time
+
+import requests
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class HN:
+    NEW_URL = 'https://hacker-news.firebaseio.com/v0/newstories.json'
+    ITEM_URL = 'https://hacker-news.firebaseio.com/v0/item/%s.json'
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.mem = configparser.ConfigParser()
+        self.mem.read('memory.ini')
+        if 'memory' not in self.mem:
+            self.mem['memory'] = {}
+        if 'status' not in self.mem:
+            self.mem['status'] = dict(last_gc=0)
+
+    def request(self, url):
+        logger.info('requesting url: %s', url)
+        data = self.session.get(url).text
+        return json.loads(data)
+
+    @staticmethod
+    def clean_text(text):
+        return text.replace('<p>', '\n')
+
+    def _get_story_memory(self, story_id):
+        # logger.info('retrieving from memory: %s', story_id)
+        try:
+            value, _ = self.mem['memory'][story_id].split(',')
+        except (ValueError, KeyError):
+            value = 'new'
+        return value
+
+    def _put_story_memory(self, story_id, value):
+        # logger.info('saving to memory: %s=%s', story_id, value)
+        self.mem['memory'][str(story_id)] = '%s,%s' % (value, int(time()))
+
+    def _flush_memory(self):
+        if int(self.mem['status']['last_gc']) + 60 * 30 < time():
+            self._gc_memory()
+
+        with open('memory.ini', 'w', encoding='utf8') as fp:
+            self.mem.write(fp)
+
+    def _gc_memory(self):
+        logger.info('running gc')
+        for key in self.mem['memory']:
+            _, time_ = self.mem['memory'][key].split(',')
+            # garbage collect this one if it's older than 2 days
+            if int(time_) + 60 * 60 * 24 * 3 < time():
+                logger.info('%s: garbage collecting', key)
+                del self.mem['memory'][key]
+                continue
+        self.mem['status']['last_gc'] = str(int(time()))
+
+    def get_updates(self):
+        story_ids = [str(x) for x in self.request(self.NEW_URL)]
+
+        for key in self.mem['memory']:
+            state = self._get_story_memory(key)
+
+            if key in story_ids:
+                story_ids.remove(key)
+                if state == 'dead':
+                    # this one was dead and now it is not
+                    logger.info('%s: story resurrected', key)
+                    story = self.request(self.ITEM_URL % key)
+                    yield (story, 'resurrected')
+                elif state == 'old':
+                    # benjamin button vibes
+                    pass
+                self._put_story_memory(key, 'new')
+                continue
+
+            if state != 'new':
+                # we'll save these for now until they are garbage collected
+                continue
+
+            logger.info('%s: story disappeared...', key)
+            story = self.request(self.ITEM_URL % key)
+            if not story:
+                # some stories are on the list of new stories but they
+                # never get pushed to the api; the api just returns "null"
+                logger.info('%s: nulled?', key)
+                self._put_story_memory(key, 'old')
+                continue
+
+            # find out why this story vanished
+            if story.get('dead'):
+                # got killed
+                logger.info('%s: ...because it was killed', key)
+                yield (story, 'killed')
+                self._put_story_memory(key, 'dead')
+            elif story.get('deleted'):
+                # was deleted by the user
+                logger.info('%s: ...because it was deleted', key)
+                # yield (story, 'deleted')
+                self._put_story_memory(key, 'deleted')
+            else:
+                # this one got too old so it does not show on the list
+                # of new stories.
+                logger.info('%s: ...because it is too old', key)
+                self._put_story_memory(key, 'old')
+
+        for story_id in story_ids:
+            # the ones that weren't in memory are new
+            logger.info('%s: adding new story', story_id)
+            self._put_story_memory(story_id, 'new')
+
+        self._flush_memory()
