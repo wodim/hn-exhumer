cd16ebdb083505da6373160d326e23801952b1f8
wodim <wodim@vortigaunt.net>
2021-11-16 11:46:09 +0100

    some fixes to the database

--- memory.py
+++ memory.py
@@ -8,14 +8,20 @@ logger = logging.getLogger(__name__)
 
 
 class Memory:
-    EXPIRY = 60 * 60 * 24
+    EXPIRY = 60 * 60 * 24 * 3
+    mem = {}
+    persistent = False
 
-    def __init__(self):
+    def __init__(self, persistent=True):
+        if not persistent:
+            return
         try:
             with open('memory.pickle', 'rb') as fp:
                 self.mem = pickle.load(fp)
-        except:
-            self.mem = {}
+            self.persistent = True
+        except Exception:
+            logger.exception('error loading pickled database! starting with an empty one.')
+            logger.warning("I won't persist memory to disk - all your data will be lost")
 
     def get_value(self, story_id, key):
         logger.debug('retrieving from memory: %s->%s', story_id, key)
@@ -28,7 +34,7 @@ class Memory:
         logger.debug('retrieving last from memory: %s->%s', story_id, key)
         try:
             return self.mem[story_id][key][-1]
-        except KeyError:
+        except (IndexError, KeyError):
             return None
 
     def put_value(self, story_id, key, value):
@@ -38,28 +44,31 @@ class Memory:
         self.mem[story_id][key] = value
         self.mem[story_id]['time'] = time()
 
-    def put_set_value(self, story_id, key, value):
-        logger.debug('saving to set memory: %s->%s=%s', story_id, key, value)
+    def put_list_value(self, story_id, key, value, limit=None):
+        logger.debug('saving to list memory: %s->%s=%s', story_id, key, value)
         if story_id not in self.mem:
             self.mem[story_id] = {}
         if key not in self.mem[story_id]:
             self.mem[story_id][key] = [value]
         else:
             self.mem[story_id][key].append(value)
+            if limit:
+                # if a limit of values was specified, truncate the list
+                # pylint: disable=E1130
+                self.mem[story_id][key] = self.mem[story_id][key][-limit:]
         self.mem[story_id]['time'] = time()
 
-    def get_all_ids(self):
-        logger.debug('getting all ids')
+    def get_all_keys(self):
+        logger.debug('getting all keys')
         return self.mem.keys()
-
-    def rank_history(self, story_id):
-        logger.debug('getting rank history for %s', story_id)
-        try:
-            return self.mem[story_id]['rank']
-        except KeyError:
-            return []
 
     def flush_memory(self):
         logger.debug('flushing memory')
+        for story_id in list(story_id for story_id in self.mem
+                             if self.mem[story_id]['time'] < time() - self.EXPIRY):
+            del self.mem[story_id]
+        if not self.persistent:
+            logger.info('WARNING: memory is not persistent - all your data will be lost!')
+            return
         with open('memory.pickle', 'wb') as fp:
             pickle.dump(self.mem, fp)
